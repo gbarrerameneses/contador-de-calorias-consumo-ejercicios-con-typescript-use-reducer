f9c26bc8923bc0f667ab25db45f778288664782d
Gustavo Barrera <gustavo.8805@gmail.com>
2024-04-15 11:26:46 -0600

    mostrando calorías consumidas & calorías quemadas

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react"
 import type { Activity } from "../types"
 
 type CalorieTrackerProps = {
@@ -5,9 +6,27 @@ type CalorieTrackerProps = {
 }
 
 export default function CalorieTracker({activities} : CalorieTrackerProps) {
-  return (
-    <div>
-      Calorie Tracker
-    </div>
+
+    // contadores
+    const caloriesConsumed = useMemo(() => activities.reduce((total, activity) =>
+            activity.category === 1 ? total + activity.calories : total, 0), [activities])
+
+    const caloriesBurned = useMemo(() => activities.reduce((total, activity) =>
+        activity.category === 2 ? total + activity.calories : total, 0), [activities])
+
+    return (
+    <>
+      <h2 className="text-4xl font-black text-white text-center">Resumen de Calorías</h2>
+        <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-5">
+            <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+                <span className="font-black text-6xl text-orange">{caloriesConsumed}</span>
+                Consumidas
+            </p>
+            <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+                <span className="font-black text-6xl text-orange">{caloriesBurned}</span>
+                Ejercicio
+            </p>
+        </div>
+    </>
   )
 }
